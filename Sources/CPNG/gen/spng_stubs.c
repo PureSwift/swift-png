8f0a85bd87a369a73c7ffc28b58115bd578ec8e3
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-30 08:13:56 -0400

    Regenerate the stubs
    98 of the 256 published functions still report that they are not implemented.

--- Sources/CPNG/gen/spng_stubs.c
+++ Sources/CPNG/gen/spng_stubs.c
@@ -305,18 +305,6 @@ png_set_alpha_mode_fixed(png_structrp png_ptr, int mode, png_fixed_point output_
 }
 
 void PNGAPI
-png_set_background(png_structrp png_ptr, png_const_color_16p background_color, int background_gamma_code, int need_expand, double background_gamma)
-{
-   png_error(png_ptr, "png_set_background is not implemented");
-}
-
-void PNGAPI
-png_set_background_fixed(png_structrp png_ptr, png_const_color_16p background_color, int background_gamma_code, int need_expand, png_fixed_point background_gamma)
-{
-   png_error(png_ptr, "png_set_background_fixed is not implemented");
-}
-
-void PNGAPI
 png_set_cHRM_XYZ(png_const_structrp png_ptr, png_inforp info_ptr, double red_X, double red_Y, double red_Z, double green_X, double green_Y, double green_Z, double blue_X, double blue_Y, double blue_Z)
 {
    png_error(png_ptr, "png_set_cHRM_XYZ is not implemented");
